32b63a92acc386103db0489e0a19b286cac27fb7
Szabo <dszabo@itron.com>
2021-06-22 11:03:47 +0200

    Initialized repo

---
+++ README.md
@@ -0,0 +1,5 @@
+### Welcome
+
+This is the supporting material for my course on [**gevent**](https://www.gevent.org/intro.html).
+
+Gevent is a coroutine -based Python networking library that uses greenlet to provide a high-level synchronous API on top of the libev or libuv event loop.
---
+++ hello_world.py
@@ -0,0 +1,18 @@
+from gevent import monkey
+import gevent
+monkey.patch_all()
+import requests
+
+urls = [
+    'https://www.google.com/',
+    'https://www.apple.com/',
+    'https://www.python.org/'
+]
+
+def response(url):
+    print(f"Working on url: {url}")
+    print(f"Response: {requests.get(url = url)}")
+
+jobs = [gevent.spawn(response, _url) for _url in urls]
+
+gevent.wait(jobs)
